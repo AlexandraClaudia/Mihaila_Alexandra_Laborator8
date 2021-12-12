d5ab9d55b3b673bab723aae4808ce9e5be89fc86
ALEXANDRA-CLAUDIA MIHĂILĂ <alexandra.claudia.mihaila@stud.ubbcluj.ro>
2021-12-12 22:56:27 +0200

    Am rezolvat laboratorul 9

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Mihaila_Alexandra_Laborator8.Models;
 
 namespace Mihaila_Alexandra_Laborator8.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Mihaila_Alexandra_Laborator8.Data.Mihaila_Alexandra_Laborator8Context _context;
 
@@ -21,11 +21,44 @@ namespace Mihaila_Alexandra_Laborator8.Pages.Books
 
         public IActionResult OnGet()
         {
+            ViewData["PublisherID"] = new SelectList(_context.Publisher, "ID", "PublisherName");
+
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+
+            PopulateAssignedCategoryData(_context, book);
+
             return Page();
         }
 
         [BindProperty]
         public Book Book { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
+        {
+            var newBook = new Book();
+            if (selectedCategories != null)
+            {
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
+            }
+
+            if (await TryUpdateModelAsync<Book>( newBook, "Book", i => i.Title, i => i.Author, i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                _context.Book.Add(newBook);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            PopulateAssignedCategoryData(_context, newBook);
+            return Page();
+        }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
